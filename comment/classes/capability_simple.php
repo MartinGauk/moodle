c0dd3fe1d1405620545ea6ed1af885f04a58810a
Martin Gauk <gauk@math.tu-berlin.de>
2021-06-22 13:13:54 +0200

    bitmask for post modes

--- comment/classes/capability_simple.php
+++ comment/classes/capability_simple.php
@@ -52,6 +52,9 @@ class capability_simple extends capability {
     /** @var bool user is allowed to post under a pseudonym */
     protected $allowpseudonym;
 
+    /** @var int allowed post modes (bitmask of the capability::POST_* constants) */
+    protected $allowedpostmodes;
+
     /** @var bool user is allowed to subscribe to the section and comments */
     protected $allowsubscriptions;
 
@@ -66,19 +69,23 @@ class capability_simple extends capability {
      * @param bool $canview Is the user allowed to view this comment section?
      * @param bool $allowreplies Is the user allowed to reply to other comments in this section?
      * @param bool $allowupvotes Is the user allowed to upvote other comments?
-     * @param bool $allowrealname Is the user allowed to post comments under their real name?
-     * @param bool $allowpseudonym Is the user allowed to post comments under a pseudonym?
+     * @param int $allowedpostmodes bitmask of the capability::POST_* constants (defining whether the user can post under
+     *     the real name and/or under a pseudonym)
      * @param bool $allowsubscriptions Is the user allowed to change the subscription to the section?
+     * @throws \coding_exception
      */
     public function __construct(section $section, \stdClass $user, bool $canview, bool $allowreplies = false,
-            bool $allowupvotes = true, bool $allowrealname = true, bool $allowpseudonym = false, bool $allowsubscriptions = true) {
+            bool $allowupvotes = true, int $allowedpostmodes = self::POST_REALNAME, bool $allowsubscriptions = true) {
         parent::__construct($section, $user);
-        $this->context = $section->get_context() && has_capability('moodle/comment:view', $this->context, $this->user);
-        $this->canview = $canview;
+        $this->context = $section->get_context();
+        $this->canview = $canview && has_capability('moodle/comment:view', $this->context, $this->user);
         $this->allowreplies = $allowreplies;
         $this->allowupvotes = $allowupvotes;
-        $this->allowrealname = $allowrealname;
-        $this->allowpseudonym = $allowpseudonym;
+        $this->allowedpostmodes = $allowedpostmodes;
+        $this->allowrealname = ($allowedpostmodes & self::POST_REALNAME) &&
+            has_capability('moodle/comment:post', $this->context, $this->user);
+        $this->allowpseudonym = ($allowedpostmodes & self::POST_PSEUDONYM) &&
+            has_capability('moodle/comment:postpseudonym', $this->context, $this->user);
         $this->allowsubscriptions = $allowsubscriptions;
     }
 
@@ -87,14 +94,13 @@ class capability_simple extends capability {
         return $this->canview;
     }
 
-    public function can_post_realname(?comment $replyto = null) : bool {
-        return $this->canview && $this->allowrealname &&
-            ($replyto === null || ($this->allowreplies && $replyto->get_replytoid() === null));
-    }
-
-    public function can_post_pseudonym(?comment $replyto = null) : bool {
-        return $this->canview && $this->allowpseudonym &&
-            ($replyto === null || ($this->allowreplies && $replyto->get_replytoid() === null));
+    public function can_post(int $postmode, ?comment $replyto = null) : bool {
+        // Check that replies are allowed when trying to reply someone. Do not allow replies to replies.
+        // Check that the requested post mode is allowed.
+        return $this->canview && ($replyto === null || ($this->allowreplies && $replyto->get_replytoid() === null)) && (
+            ($postmode === self::POST_REALNAME && $this->allowrealname) ||
+            ($postmode === self::POST_PSEUDONYM && $this->allowpseudonym)
+        );
     }
 
     public function can_edit(comment $comment = null) : bool {
